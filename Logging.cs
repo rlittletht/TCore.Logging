9ecd5acb29cf1e91f4488cb7c9f2b1141df0f496
rlittletht <rlittle@thetasoft.com>
2015-09-18 23:58:04 -0700

    add traceevent and remove scaffolding for complete logging

--- Logging.cs
+++ Logging.cs
@@ -49,7 +49,7 @@ namespace TCore.Logging
             }
         }
 
-        Guid ID => m_guidCorrelation;
+        public Guid Crids => m_guidCorrelation;
 
         public CorrelationID()
         {
@@ -62,6 +62,13 @@ namespace TCore.Logging
             m_cridParent = cridParent;
         }
 
+        public static CorrelationID FromCrids(Guid crids)
+        {
+            CorrelationID crid = new CorrelationID();
+            crid.m_guidCorrelation = crids;
+            return crid;
+        }
+
         public static CorrelationID Create(CorrelationID cridParent = null)
         {
             CorrelationID crid = new CorrelationID();
@@ -71,6 +78,15 @@ namespace TCore.Logging
             return crid;
         }
     }
+
+    public enum EventType
+    {
+        Critical = TraceEventType.Critical,
+        Error = TraceEventType.Error,
+        Warning = TraceEventType.Warning,
+        Information = TraceEventType.Information,
+        Verbose = TraceEventType.Verbose
+    };
 
     public class LogProvider
     {
@@ -86,11 +102,12 @@ namespace TCore.Logging
 
         public LogProvider(string sFile)
         {
+            m_ts = new TraceSource(sFile);
+#if TESTPROVIDER
             string s = String.Format("foo{0}{1}", "1", 2);
             test("foo{0}{1}", "1", 2);
             test("foo");
 
-            m_ts = new TraceSource(sFile);
             if (m_ts.Switch.ShouldTrace(TraceEventType.Information))
                 m_ts.TraceInformation("test");
             if (m_ts.Switch.ShouldTrace(TraceEventType.Critical))
@@ -98,17 +115,18 @@ namespace TCore.Logging
             if (m_ts.Switch.ShouldTrace(TraceEventType.Error))
                 m_ts.TraceInformation("test");
             m_ts.TraceEvent(TraceEventType.Information, 12345, "test{0}", "foo");
+#endif
             m_sFile = sFile;
         }
 
         private const string _sGuidZero = "00000000-0000-0000-0000-000000000000";
 
-        private static void LogInternal(TraceSource ts, CorrelationID crid, int nTicks, DateTime dttm, string s, params object[] rgo)
+        private static void LogInternal(TraceSource ts, CorrelationID crid, EventType et, int nTicks, DateTime dttm, string s, params object[] rgo)
         {
-            if (ts.Switch.ShouldTrace(TraceEventType.Information))
+            if (ts.Switch.ShouldTrace((TraceEventType) et))
                 {
                 string sFormatted = String.Format(s, rgo);
-                ts.TraceEvent(TraceEventType.Information, crid?.Hash2 ?? 0, "{0}\t{1}\t{2:X8}\t{3}\t{4}", crid?.Text ?? _sGuidZero, System.Threading.Thread.CurrentThread.ManagedThreadId, nTicks, dttm, sFormatted);
+                ts.TraceEvent((TraceEventType)et, crid?.Hash2 ?? 0, "{0}\t{1}\t{2:X8}\t{3}\t{4}", crid?.Text ?? _sGuidZero, System.Threading.Thread.CurrentThread.ManagedThreadId, nTicks, dttm, sFormatted);
                 }
         }
 
@@ -122,7 +140,7 @@ namespace TCore.Logging
         static public void LogSzUnsafe(CorrelationID crid, string s, string sFile)
         {
             TraceSource ts = new TraceSource(sFile);
-            LogInternal(ts, crid, Environment.TickCount, DateTime.Now, s);
+            LogInternal(ts, crid, EventType.Information, Environment.TickCount, DateTime.Now, s);
             ts.Close();
         }
 
@@ -136,8 +154,18 @@ namespace TCore.Logging
         static public void LogSzUnsafe(DateTime dttm, int nTicks, string s, string sFile)
         {
             TraceSource ts = new TraceSource(sFile);
-            LogInternal(ts, null, nTicks, dttm, s);
+            LogInternal(ts, null, EventType.Information, nTicks, dttm, s);
             ts.Close();
+        }
+
+        public void LogEvent(CorrelationID crid, EventType et, string s, params object[] rgo)
+        {
+            if (!m_ts.Switch.ShouldTrace((TraceEventType) et))
+                return;
+
+            int nTicks = Environment.TickCount;
+            DateTime dttm = DateTime.Now;
+            LogInternal(m_ts, crid, et, nTicks, dttm, s, rgo);
         }
 
         /* L O G  S Z */
@@ -149,12 +177,7 @@ namespace TCore.Logging
         ----------------------------------------------------------------------------*/
         public void LogSz(CorrelationID crid, string s, params object[] rgo)
         {
-            if (!m_ts.Switch.ShouldTrace(TraceEventType.Information))
-                return;
-
-            int nTicks = Environment.TickCount;
-            DateTime dttm = DateTime.Now;
-            LogInternal(m_ts, crid, nTicks, dttm, s, rgo);
+            LogEvent(crid, EventType.Information, s, rgo);
         }
 
     }
