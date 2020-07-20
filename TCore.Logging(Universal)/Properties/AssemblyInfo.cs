aaae17548c769e60795fb32eeebfba4bc5b75129
Rob Little <rlittletht@users.noreply.github.com>
2020-07-19 19:27:22 -0700

    Add solution for building all projects, update nuget publish specs

--- TCore.Logging(Universal)/Properties/AssemblyInfo.cs
+++ TCore.Logging(Universal)/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TCore.Logging(Universal)")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TCore.Logging")]
+[assembly: AssemblyDescription("Tcore.Logging")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TCore.Logging(Universal)")]
+[assembly: AssemblyProduct("TCore.Logging")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
 [assembly: ComVisible(false)]
